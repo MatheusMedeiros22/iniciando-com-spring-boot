c11117c5ad17947d2163e77c8912b9f69b21fe08
Matheus Medeiros <mathmed22@gmail.com>
2022-06-17 15:16:46 -0300

    Project created and methods GET - POST done

---
+++ src/main/java/academy/devdojo2/springboot2essentials2/controller/AnimeController.java
@@ -0,0 +1,36 @@
+package academy.devdojo2.springboot2essentials2.controller;
+
+import academy.devdojo2.springboot2essentials2.domain.Anime;
+import academy.devdojo2.springboot2essentials2.service.AnimeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("animes")
+public class AnimeController {
+    private AnimeService animeService;
+
+    public AnimeController(AnimeService animeService){
+        this.animeService = animeService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Anime>> list(){
+        return new ResponseEntity<>(animeService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Anime> findById(@PathVariable long id){
+        return new ResponseEntity<>(animeService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Anime> save(@RequestBody Anime anime){
+        return new ResponseEntity<>(animeService.save(anime), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/academy/devdojo2/springboot2essentials2/domain/Anime.java
@@ -0,0 +1,12 @@
+package academy.devdojo2.springboot2essentials2.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Anime {
+
+    private long id;
+    private String name;
+}
